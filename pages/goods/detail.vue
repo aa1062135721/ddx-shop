bac0242b76917c718243f6081cb78d91b785c3eb
吴黎明 <1375565766@qq.com>
2019-08-29 11:04:53 +0800

    商品详情页

--- pages/goods/detail.vue
+++ pages/goods/detail.vue
@@ -118,22 +118,90 @@
 					<view class="iconfont icon-ddx-shop-shopping"></view>
 					<view class="text">商城</view>
 				</view>
-				<view class="box">
+				<view class="box" @click="open()">
 					<view class="iconfont icon-ddx-shop-shopping-cart-o"></view>
 					<view class="text">购物车</view>
+					<view class="number">11</view>
 				</view>
 			</view>
 			<view class="btn">
 				<view class="joinCart">加入购物车</view>
 				<view class="buy" @tap="buy">立即购买</view>
+				<!-- <view class='over'>已售完</view> -->
 			</view>
 		</view>
+
+		<!-- 购买的时候选择规格，数量…… -->
+		<uni-popup ref="selectSpecification" type="bottom" :custom="true" @change="change">
+			<view class="select-specification">
+				<view class="goods-info">
+					<view class="main">
+						<view class="image">
+							<image class="img" src='../../static/images/goods.jpg'></image>
+						</view>
+						<view class="info">
+							<view class="price">￥398</view>
+							<view class="stock">库存充足</view>
+							<view class="chooses">已选：黄色,S</view>
+						</view>
+						<view class="close">
+							<text class="iconfont icon-ddx-shop-shopping"></text>
+						</view>
+					</view>
+				</view>
+				<view class="specification">
+					<view class="main">
+						<view class="title">
+							主要颜色
+						</view>
+						<view class="content">
+							<text class="on">黄色</text>
+							<text>白色</text>
+							<text>其他</text>
+							<text>水水水水</text>
+							<text>黄色</text>
+							<text>白色</text>
+							<text>其他</text>
+						</view>
+					</view>
+					<view class="main">
+						<view class="title">
+							尺码
+						</view>
+						<view class="content">
+							<text>XS</text>
+							<text class="on">S</text>
+							<text>M</text>
+						</view>
+					</view>
+				</view>
+				<view class="buy-num">
+					<view class="main">
+						<view class="title">
+							购买数量
+						</view>
+						<view class="content">
+							<uni-number-box :min="1" :value="1" :step="1"></uni-number-box>
+						</view>
+					</view>
+				</view>
+				<view class="btns">
+					<view class="btn" style="background:#FC8A8A;">
+						加入购物车
+					</view>
+					<view class="btn">
+						立即购买
+					</view>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
 <script>
 	import uniNumberBox from "@/components/uni-number-box/uni-number-box.vue"
 	import separator from "@/components/separator.vue"
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
 	export default {
 		data() {
 			return {
@@ -156,7 +224,8 @@
 				anchorlist:[],//导航条锚点
 				selectAnchor:0,//选中锚点
 
-				descriptionStr:'<div style="text-align:center;"><img width="100%" src="https://ae01.alicdn.com/kf/HTB1t0fUl_Zmx1VjSZFGq6yx2XXa5.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB1LzkjThTpK1RjSZFKq6y2wXXaT.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB18dkiTbvpK1RjSZPiq6zmwXXa8.jpg"/></div>'
+				descriptionStr:'<div style="text-align:center;"><img width="100%" src="https://ae01.alicdn.com/kf/HTB1t0fUl_Zmx1VjSZFGq6yx2XXa5.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB1LzkjThTpK1RjSZFKq6y2wXXaT.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB18dkiTbvpK1RjSZPiq6zmwXXa8.jpg"/></div>',
+
 			}
 		},
 		methods:{
@@ -199,6 +268,14 @@
 			back() {
 				uni.navigateBack();
 			},
+			//打开选择规格弹框
+			open(){
+				this.$refs.selectSpecification.open()
+			},
+			//选择规格
+			change(e) {
+				console.log(e.show)
+			},
 		},
 		onLoad(option) {
 		},
@@ -220,6 +297,7 @@
 		components: {
 			uniNumberBox,
 			separator,
+			uniPopup,
 		}
 	}
 </script>
@@ -475,17 +553,17 @@
 		.icons {
 			display: flex;
 			justify-content: center;
-			height: 80upx;
+			height: 98upx;
 			width: 30%;
 			.box {
 				position: relative;
-				width: 80upx;
-				height: 80upx;
+				width: 98upx;
+				height: 98upx;
 				display: flex;
 				justify-content: center;
 				flex-wrap: wrap;
 				.iconfont {
-					font-size: 50upx;
+					font-size: 60upx;
 					color: #828282;
 				}
 				.text {
@@ -496,8 +574,16 @@
 					color: #666;
 				}
 				.number{
+					background: $color-primary;
+					border-radius:50%; 
+					color: #fff;
+					font-size: $uni-font-size-sm;
+					width: 34upx;
+					height: 34upx;
+					text-align: center;
+					line-height: 34upx;
 					position: absolute;
-					top: 0;
+					top: 4upx;
 					right: 0;
 				}
 			}
@@ -509,7 +595,8 @@
 			display: flex;		
 			justify-content: flex-end;
 			.joinCart,
-			.buy {
+			.buy,
+			.over {
 				height: 98upx;
 				width: 250upx;
 				line-height: 98upx;
@@ -523,7 +610,137 @@
 			.buy {
 				background-color: #FC5A5A;
 			}
+			.over{
+				background:#666666;
+				width:100%;
+			}
 		}
 	}
+
+	/*  选择规格，数量后加入该购物车或者立即购买 */ 
+	.select-specification{
+		background: #fff;
+		.goods-info{
+			width: 100%;
+			padding: 0 $uni-spacing-col-base;
+			.main{
+				padding-top: $uni-spacing-col-base;
+				padding-bottom: $uni-spacing-col-base;
+				border-bottom:1upx #E4E4E4 solid;
+				display: flex;
+				flex-wrap: nowrap;
+				justify-content: space-between;
+				.image{
+					width: 30%;
+					height: 100%;
+					.img{
+						width: 208upx;
+						height: 208upx;
+						border-radius: 4upx;
+					}
+				}
+				.info{
+					width: 60%;
+					height: 100%;
+					padding-left:$uni-spacing-col-base; 
+					display: flex;
+					flex-direction: column;
+					justify-content: flex-end;
+					.price{
+						color: $color-primary;
+						font-size: $uni-font-size-lg;
+					}
+					.stock{
+						color: #808080;
+						font-size: $uni-font-size-base;
+					}
+					.chooses{
+						color: $color-primary-plain;
+						font-size: $uni-font-size-lg;
+					}
+				}
+				.close{
+					width: 10%;
+					height: 100%;
+					position: relative;
+					.iconfont{
+						position: absolute;
+						top: 0;
+						right: 0;
+						font-size: $uni-font-size-lg + $uni-font-size-sm;
+					}
+				}
+			}
+			
+		}
+		.specification{
+			width: 100%;
+			padding:0 $uni-spacing-col-base;
+			.main{
+				padding-top: $uni-spacing-col-base;
+				padding-bottom: $uni-spacing-col-base;
+				border-bottom:1upx #E4E4E4 solid;				
+				.title{
+					color: $color-primary-plain;
+					font-size: $uni-font-size-lg;
+					padding-bottom: $uni-spacing-col-base;
+				}
+				.content{
+					display: flex;
+					flex-direction: row;
+					justify-content: flex-start;
+					flex-wrap: wrap;
+					text{
+						background: #F2F2F2;
+						border: 1upx solid #F2F2F2;
+						color: $color-primary-plain;
+						font-size: $uni-font-size-base;
+						margin-right: 20upx;
+						margin-bottom: 20upx;
+						padding:10upx 24upx;
+						border-radius:4upx;
+						&.on{
+							color: $color-primary;
+							border: 1upx solid $color-primary;
+							background: #FAE8E8;
+						} 
+					}
+				}
+			}
+		}
+		.buy-num{
+			width: 100%;
+			margin-bottom: 20upx;
+			.main{
+				padding: $uni-spacing-col-base;
+				display: flex;
+				justify-content: space-between;
+				.title{
+					color: $color-primary-plain;
+					font-size: $uni-font-size-lg;
+					padding-bottom: $uni-spacing-col-base;
+				}
+				.content{
+					font-size: $uni-font-size-sm;
+				}
+			}
+		}
+		.btns{
+			width: 100%;
+			height: 98upx;
+			display: flex;
+			justify-content:space-between;
+			color: #fff;
+			.btn{
+				background: $color-primary;
+				font-size: $uni-font-size-lg;
+				text-align: center;
+				height: 98upx;
+				line-height: 98upx;	
+				width: 50%;
+			}
+		}
+	}
+
 }
 </style>

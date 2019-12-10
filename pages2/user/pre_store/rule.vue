d47b872fb8bdee30afe8e13dd3654131d8bf45f7
吴黎明 <1375565766@qq.com>
2019-12-10 21:58:30 +0800

    充值送累计送 活动细则

--- pages2/user/pre_store/rule.vue
+++ pages2/user/pre_store/rule.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="recharge">
+<!--
     <div class="privilege">
       <div class="privilege-title" style='color: #333333;'>活动介绍</div>
       <div class="privilege-box">
@@ -23,25 +24,35 @@
         </div>
       </div>
     </div>
+ -->
     <div class="privilege">
       <div class="privilege-title">活动细则</div>
       <div class="privilege-box">
         <div class="privilege-box-item">
-          <div class="right">1. 本活动预存金额仅限捣蛋熊猫线上商城消费使用，消费时长无限制；</div>
+          <div class="right">1.本活动预存金额仅限捣蛋熊猫线上商城消费使用，消费时长无限制。如有商品退货，退款仅能退入商城会员卡内，但预存金额不可退。</div>
         </div>
         <div class="privilege-box-item">
-          <div class="right">2. 参与本活动获得的赠礼，从办理本活动次日（遇非工作日顺延）开始计算，限时30天内使用完。</div>
+          <div class="right">2.参与本活动获得的赠礼，从发放赠礼当日开始计算（达标后2日内发送赠礼且不作另行通知，敬请关注会员账户并注意查收），限时30天内使用完毕。逾期不领取或不使用，视作顾客自动放弃，后期不补赠。</div>
         </div>
         <div class="privilege-box-item">
-          <div class="right">3. 赠礼可转赠亲友，但不可兑换现金，不能延期，逾期不可使用；</div>
+          <div class="right">3.累计消费以“连续一个月”作为是否达标计算单位（如：1月5日第一次消费即到2月5日即为连续一个月）。参与所有活动预存的金额，消费后均不能计入本次累消活动，不能获得累消赠礼。</div>
         </div>
         <div class="privilege-box-item">
-          <div class="right">4. 双胞胎可共用赠礼，但赠送次数为一个宝贝15次。（注：其他双孩家庭不可共用赠礼）；</div>
+          <div class="right">4.赠礼可转赠亲友，但不可兑换现金，不能延期，逾期不可使用。</div>
         </div>
         <div class="privilege-box-item">
-          <div class="right">5. 线上会员福利以“熊猫商城”会员福利和会员规则为准。</div>
+          <div class="right">5.双胞胎可共用赠礼，两个宝贝需平分赠礼。如果赠礼是不限次数的情况，则一个宝贝15次。（注：其他双孩家庭不可共用赠礼）</div>
+        </div>
+        <div class="privilege-box-item">
+          <div class="right">6.使用赠礼前请先领取“活动专用水育卡”，在门店使用时需出示“核销码”并登记宝宝姓名和家长电话。（注：请家长自行携带游泳裤，门店一次性游泳裤需另行收费。）</div>
+        </div>
+        <div class="privilege-box-item">
+          <div class="right">7.其他线上会员福利以“熊猫商城”会员福利和会员规则为准。</div>
         </div>
       </div>
+    </div>
+    <div class="my-footer">
+      最终解释权归重庆捣蛋熊科技有限公司所有
     </div>
   </div>
 </template>
@@ -91,5 +102,11 @@ export default {
       }
     }
   }
+}
+.my-footer{
+  color: #999999;
+  padding: 38upx 0;
+  text-align: center;
+  width: 100%;
 }
 </style>
